61aaa238d98903871b7dd56725bb6b587db23ca1
Nyamadi Atsu <devbrainy98@gmail.com>
2024-05-11 08:42:16 +0100

    cors config setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -38,7 +38,10 @@ const initializeServer = async () => {
 
   // apply middleware
   app.use(helmet());
-  app.use(cors());
+  app.use(cors({
+    origin: [process.env.FRONTEND_URL!, "http://localhost:3000"],
+    credentials: true
+  }));
   // app.set('trust proxy', 1);
   // app.use("/api", rateLimiter);
   app.use((req, res, next) => {
